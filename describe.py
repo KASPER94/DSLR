73966576e1e50e412751312666cfab19c5e37d65
Simon Kaperski <skapersk@made-f0Br7s6.clusters.42paris.fr>
2025-04-23 17:45:21 +0200

    describe done

--- describe.py
+++ describe.py
@@ -1,19 +1,44 @@
 from load_csv import load
 import numpy as np
-from maths import count
+from maths import _count, _mean, _std, _max, _min, _percentile
 
 def start(file):
     datas = load(file)
     features = datas[0]
     dataset = datas[1:, :]
     print(f'{"":15} |{features[6]:>12} |{features[7]:>12} |{features[8]:>12} | {features[9]:>12} |')
-    all_col_data = np.array([])
-    for i in range(0, len(dataset)):
-        try:
-            value = float(dataset[i, 6])
-            if not np.isnan(value):
-                all_col_data = np.append(all_col_data, value)
-            # print(f'{"":15} | {count(data):>12.4f}')
-        except:
-            print("error")
-    print(count(all_col_data))
+    count = []
+    mean = []
+    Std = []
+    Min = []
+    Max = []
+    twentyFive = []
+    fifty = []
+    sevFiv = []
+    for col in range(6, 10):
+        all_col_data = np.array([])
+        
+        for i in range(0, len(dataset)):
+            try:
+                value = float(dataset[i, col])
+                if not np.isnan(value):
+                    all_col_data = np.append(all_col_data, value)
+            except:
+                pass 
+        
+        count.append(_count(all_col_data))
+        mean.append(_mean(all_col_data))
+        Std.append(_std(all_col_data))
+        Min.append(_min(all_col_data))
+        Max.append(_max(all_col_data))
+        twentyFive.append(_percentile(all_col_data, 25))
+        fifty.append(_percentile(all_col_data, 50))
+        sevFiv.append(_percentile(all_col_data, 75))
+    print(f'{"Count":15} | {count[0]:>12.2f}| {count[1]:>12.2f}| {count[2]:>12.2f}| {count[3]:>12.2f}                  |')
+    print(f'{"Mean":15} | {mean[0]:>12.2f}| {mean[1]:>12.2f}| {mean[2]:>12.2f}| {mean[3]:>12.2f}                  |')
+    print(f'{"Std":15} | {Std[0]:>12.2f}| {Std[1]:>12.2f}| {Std[2]:>12.2f}| {Std[3]:>12.2f}                  |')
+    print(f'{"Min":15} | {Min[0]:>12.2f}| {Min[1]:>12.2f}| {Min[2]:>12.2f}| {Min[3]:>12.2f}                  |')
+    print(f'{"25%":15} | {twentyFive[0]:>12.2f}| {twentyFive[1]:>12.2f}| {twentyFive[2]:>12.2f}| {twentyFive[3]:>12.2f}                  |')
+    print(f'{"50%":15} | {fifty[0]:>12.2f}| {fifty[1]:>12.2f}| {fifty[2]:>12.2f}| {fifty[3]:>12.2f}                  |')
+    print(f'{"75%":15} | {sevFiv[0]:>12.2f}| {sevFiv[1]:>12.2f}| {sevFiv[2]:>12.2f}| {sevFiv[3]:>12.2f}                  |')
+    print(f'{"Max":15} | {Max[0]:>12.2f}| {Max[1]:>12.2f}| {Max[2]:>12.2f}| {Max[3]:>12.2f}                  |')
--- maths.py
+++ maths.py
@@ -1,9 +1,55 @@
 import numpy as np
 
-def count(X):
+def _count(X):
     try:
         X = np.astype('float')
         X = X[~np.isnan(X)]
         return len(X)
     except :
         return len(X)
+    
+def _mean(X):
+    add = 0
+    for x in X:
+        if np.isnan(x):
+            continue
+        add += x
+    return add/len(X)
+
+def _std(X):
+  mean = _mean(X)
+  total = 0
+  for x in X:
+    if np.isnan(x):
+      continue
+    total = total + (x - mean) ** 2
+  return (total / len(X)) ** 0.5
+
+def _min(X):
+  min_value = X[0]
+  for x in X:
+    val = x
+    if val < min_value:
+      min_value = val
+  return min_value
+
+def _max(X):
+  min_value = X[0]
+  for x in X:
+    val = x
+    if val > min_value:
+      min_value = val
+  return min_value
+
+def _percentile(X, p):
+  X.sort()
+  k = (len(X) - 1) * (p / 100)
+  f = np.floor(k)
+  c = np.ceil(k)
+
+  if f == c:
+    return X[int(k)]
+
+  d0 = X[int(f)] * (c - k)
+  d1 = X[int(c)] * (k - f)
+  return d0 + d1
