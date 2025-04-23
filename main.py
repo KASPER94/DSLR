28fcfedd78bf6917f6dda6a48aeeb1b08323e05e
Simon Kaperski <skapersk@made-f0Br7s6.clusters.42paris.fr>
2025-04-23 17:55:09 +0200

    starting hist etc

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ from describe import start
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--describe', type=str, help='Prog to describe dataset as a paramete')
+    parser.add_argument('-v', '--visualization', type=str, help='Prog to describe dataset as a paramete')
     args = parser.parse_args()
 
     if (args.describe):
         start(args.describe)
+    if (args.visualization):
+        match args.visualization:
+            case "Histogram":
+                print("HELL")
+                pass
+            case "Scatter":
+                pass
+            case "Pair":
+                pass
 
 if __name__ == "__main__":
     main()
